9c02f68bea79601c7942dc4e98631a4a17094db4
Erik Solis <solie2@icloud.com>
2024-03-17 21:37:15 -0400

    Defined a function that prints the rules of the game

--- vingtEtUn.py
+++ vingtEtUn.py
@@ -15,17 +15,37 @@ def showRules() -> None:
     '''
     Prints the rules to the screen
     '''
+    lines = [
+        "",
+        "The goal of the game is to score 21 points, or as near as possible without going over.",
+        "The two players take turns throwing two dies as many times as desired and adding up the numbers thrown on each round.",
+        "",
+        "  • A player who totals over 21 is bust and loses the game",
+        "  • The player whose total is nearest 21, after each player has had a turn, wins the game.",
+        "  • In the case of an equality high total, the game is tied.",
+        "",
+        "The game is over at the end of a round when:",
+        "  • One or both players are bust.",
+        "  • Both players choose to stay.",
+        "",
+        "Notes",
+        "  • Once a player totals 14 or more, one of the dies is discarder for the consecutive turns.",
+        "  • The house must throw the dice until the total is 17 or higher. At 17 or higher, the house must stay.",
+        ""
+    ]
+    rules = "\n".join(lines)
+    print(rules)
     return
 
 def playGame():
-    return
+    ...
 
 
-def takeTurn(player: str, turn: int) -> dict:
-    return
+def takeTurn(turn: int, player: str = "house") -> dict:
+    ...
 
 
-def main():
+def main() -> None:
 
     # Program Banner
     banner = """
@@ -38,30 +58,30 @@ def main():
                           __/ |                                                     
                          |___/                    
     ----------------------------------------------------------------------------
-------------------------------------------------------------------------------------\n
+------------------------------------------------------------------------------------
 """
     print(banner)
     USERNAME = input("Enter player name: ").strip() # Get the user's name
-    print(f"Hello {USERNAME}!") # Greet the user
+    print(f"\nHello {USERNAME}!\n") # Greet the user
 
     while True:
-            print("1. View the Rules.\n2. Play Vingt-et-un.\n3. Quit.") # Display menu options
-            user_input = input("Choose from the  options: ").strip() # Get the player's choice
+            print("Choose from the following options:")
+            print("  1. View the Rules.\n  2. Play Vingt-et-un.\n  3. Quit.\n") # Display menu options
+            user_input = input("Select an option (1|2|3): ").strip() # Get the player's choice
             # Check to make sure the user entered a digit. This eliminates the need to set up a try-except block to catch the ValueError caused by subsequently converting the input to an integer.
             if user_input.isdigit(): 
                 choice = int(user_input)
-                if choice > 0 and choice >= 3:
+                if choice > 0 and choice <= 3:
                     if choice == 1:
                         showRules()
                     elif choice == 2:
                         playGame()
-                    elif choice:
-                        sys.exit()
-                    
+                    elif choice == 3:
+                        sys.exit()          
                 else:
-                    print(f"Error ... {choice} is not an integer in the range 1 - 3")
+                    print(f"Error ... {choice} is not an integer in the range 1 - 3\n")
             else:
-                print(f"Error ... Invalid option. Please enter 1, 2, or 3")
+                print(f"Error ... Invalid option. Please enter 1, 2, or 3\n")
     return
 
 

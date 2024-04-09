c607fb69af34ea49b1273ae3b115d2625d178c55
Erik Solis <solie2@icloud.com>
2024-04-09 19:27:22 -0400

    First Release

--- vingtEtUn.py
+++ vingtEtUn.py
@@ -5,10 +5,9 @@
 # 
 # Date Created: Sun Mar 17 01:25:17 EDT 2024
 # 
-# Last Modified: Mon Apr 8 15:49:01 EDT 2024
+# Last Modified: Tue Apr  9 18:01:38 EDT 2024
 #
 
-import sys
 import random
 
 
@@ -47,31 +46,30 @@ def throwDice(dice_count: int) -> int:
     return sum(random.randint(1,6) for i in range(dice_count))
 
 
-def playGame():
-
+def playGame(player_name: str) -> str:
+    '''
+    Plays a game of Vingt Et Un
+    '''
     user_total = 0
     house_total = 0
 
     while True:
         # The user takes their turn
-        print("Rolling dice ...")
+        print(f"Rolling dice for {player_name}...")
         if user_total < 14:
             user_total += throwDice(2)
         else:
             user_total += throwDice(1)
-        print(f"Your Total: {user_total}\n")
+        print(f"{player_name}'s Total: {user_total}\n")
 
         if user_total > 21:
-            status = "bust"
-            print(f"You {status}, House wins.\n")
-            return "house"
+            print(f"{player_name} busts\n")
+            return "House"
         elif user_total == 21:
-            status = "win"
-            print(f"You {status}!\n")
-            return "user"
+            return player_name
         
         # The house takes it's turn
-        print("Rolling dice ...")
+        print("Rolling dice for House...")
         if house_total < 17:
             if house_total < 14:
                 house_total += throwDice(2)
@@ -82,22 +80,23 @@ def playGame():
             print("House stays.\n")
         
         if house_total > 21:
-            print("House busts ... you win!\n")
-            return "user"
+            print(f"House busts\n")
+            return player_name
         elif house_total >= 17:
             if user_total > house_total:
-                print("You win!\n")
-                return "user"
+                #print(f"{player_name} wins!\n")
+                return player_name
             elif user_total < house_total:
-                print("You lose!\n")
-                return "house"
+                #print(f"{player_name} loses!\n")
+                return "House"
             else:
-                print(f"It's a tie!\n")
-                return "tie"    
+                return "Tie"    
 
 
 def main() -> None:
-
+    '''
+    Entry point to our program. Provides an menu interface for the user.
+    '''
     # Program Banner
     banner = """
  __      __  _                   _                     _                            
@@ -112,30 +111,33 @@ def main() -> None:
 ------------------------------------------------------------------------------------
 """
     print(banner)
-    USERNAME = input("Enter player name: ").strip() # Get the user's name
-    print(f"\nHello {USERNAME}!\n") # Greet the user
+    username = input("Enter player name: ").strip().title() # Get the user's name
+    print(f"\nHello {username}!\n") # Greet the user
 
     while True:
             print("Choose from the following options:")
             print("  1. View the Rules.\n  2. Play Vingt-et-un.\n  3. Quit.\n") # Display menu options
             user_input = input("Select an option (1|2|3): ").strip() # Get the player's choice
+            print("")
             # Check to make sure the user entered a digit. This eliminates the need to set up a try-except block to catch the ValueError caused by subsequently converting the input to an integer.
             if user_input.isdigit(): 
                 choice = int(user_input)
-                if choice > 0 and choice <= 3:
+                if choice > 0 and choice <= 3: # Check to make sure the digit provided is 1, 2, or 3
                     if choice == 1:
                         showRules()
                     elif choice == 2:
-                        victor = playGame()
-                        print(f"{victor.title()} wins this round!")
+                        result = playGame(player_name=username)
+                        if result != "Tie":
+                            print(f"{result} wins this round!\n")
+                        else:
+                            print("It's a Tie!\n")
                     elif choice == 3:
-                        print("Goodbye ...")
-                        sys.exit()          
+                        print(f"\nThanks for playing {username}, goodbye! ...")
+                        return       
                 else:
                     print(f"Error ... {choice} is not an integer in the range 1 - 3\n")
             else:
                 print(f"Error ... Invalid option. Please enter 1, 2, or 3\n")
-    return
 
 
 if __name__ == "__main__":
